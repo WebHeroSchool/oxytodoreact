77d2f46d60f1d0a7240421668bb4b732cd06b798
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-11-17 19:40:56 +0300

    created ItemList.module.css, changed ItemList.js,Item.module.css

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Item from '../Item/Item.js'
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
+import styles from '../ItemList/ItemList.module.css';
 
 const ItemList = ({items}) => (<ul>
-    {items.map(item =><div key={item.value}><Checkbox />
-    	<Item value={item.value} isDone={item.isDone} />
+    {items.map(item =><div className={styles.item_list} key={item.value}><Checkbox />
+    	<Item value={item.value} isDone={item.isDone} /><div className={styles.item_wrap}></div>
     <DeleteIcon /></div>)}
 </ul>);
 
