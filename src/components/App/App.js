37203f3854196c8ebb0560da065bb1c10839cebe
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-11-18 13:55:41 +0300

    changed App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,29 +5,31 @@ import Footer from '../Footer/Footer.js';
 import styles from './App.module.css';
 
 class App extends React.Component {
+    state = {
+        items:[
+                {
+                    value: 'Написать приложение',
+                    isDone: false
+                },
+                {
+                    value: 'Прочитать теорию',
+                    isDone: false
+                },
+                {
+                    value: 'Сдать задание',
+                    isDone: true
+                }
+            ]
+    };
+    
     render() {
-        const items = [
-        {
-            value: 'Написать приложение',
-            isDone: false
-        },
-        {
-            value: 'Прочитать теорию',
-            isDone: false
-        },
-        {
-            value: 'Сдать задание',
-            isDone: true
-        }
-    ];
-
-    return(
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Важные дела</h1>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count ={3} />
-    </div>);
+        return(
+            <div className={styles.wrap}>
+                <h1 className={styles.title}>Важные дела</h1>
+                <InputItem />
+                <ItemList items={this.state.items} />
+                <Footer count ={3} />
+            </div>);
     }
 };
 
