0d00ac5d0778c2643a20cb1397e27a1730fb0a8f
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-11-18 12:22:23 +0300

    deleted App.js, changed App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,29 +4,31 @@ import ItemList from '../ItemList/ItemList.js';
 import Footer from '../Footer/Footer.js';
 import styles from './App.module.css';
 
-const App = () => {
-    const items = [
+class App extends React.Component {
+    render() {
+        const items = [
         {
-        	value: 'Написать приложение',
-        	isDone: false
+            value: 'Написать приложение',
+            isDone: false
         },
         {
-        	value: 'Прочитать теорию',
-        	isDone: false
+            value: 'Прочитать теорию',
+            isDone: false
         },
         {
-        	value: 'Сдать задание',
-        	isDone: true
+            value: 'Сдать задание',
+            isDone: true
         }
     ];
 
     return(
- 	<div className={styles.wrap}>
-	  <h1 className={styles.title}>Важные дела</h1>
-	  <InputItem />
-	  <ItemList items={items} />
-	  <Footer count ={3} />
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>Важные дела</h1>
+      <InputItem />
+      <ItemList items={items} />
+      <Footer count ={3} />
     </div>);
+    }
 };
 
 export default App;
