8a3c7ffb57fb60a44badb79ee9b6866dccaa9771
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-11-18 22:00:29 +0300

    changed ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,14 +4,15 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import styles from '../ItemList/ItemList.module.css';
 
-const ItemList = ({items, onClickDone,isDone}) => (<ul>
+const ItemList = ({items, onClickDone}) => (<ul>
     {items.map(item =><div className={styles.item_list} key={item.value}>
     	<Checkbox
-    	    checked={isDone}
+    	    checked={item.isDone}
     	    tabIndex={-1}
-    	    onClick={()=>onClickDone(isDone)}
+    	    onClick={()=>onClickDone(item.isDone)}
     	/>
-    	<Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} /><div className={styles.item_wrap}></div>
+    	<Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
+    	<div className={styles.item_wrap}></div>
     <DeleteIcon /></div>)}
 </ul>);
 
