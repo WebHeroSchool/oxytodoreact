3331b3922a7ff49e49fb763c0bd25eaeded987c6
Oxana <62831704+oxyrud@users.noreply.github.com>
2020-12-21 13:21:27 +0300

    imported octokid/rest, changed About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {Octokit} from '@octokit/rest';
 
-const About = () => (<span>About</span>)
+const octokit = new Octokit();
+
+class About extends React.Component {
+	state = {
+		isLoading: true,
+		repoList: []
+	}
+
+    componentDidMount(){
+    	octokit.repos.listForUser({
+    		username: 'oxyrud'
+    	}).then(({data}) => {
+    		this.setState({
+                repoList: data,
+                isLoading: false
+    		});
+    	});
+    }
+
+	render() {
+		const {isLoading, repoList} = this.state;
+
+		return (
+            <CardContent>
+	            <h1> { isLoading ? <CircularProgress /> : 'Мои репозитории'}</h1>
+                {!isLoading && <ol>
+                	{repoList.map(repo => (<li key = {repo.id}>
+                		{repo.name}
+                	</li>))}
+                	</ol>}
+	        </CardContent>
+	    );
+	}    
+}
 
 export default About;
