e873a1d0ee98175f0deb27097c37e7b79ab4e11c
Oxana <62831704+oxyrud@users.noreply.github.com>
2021-01-17 16:00:55 +0300

    changed App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Todo from'../Todo/Todo';
 
 import About from '../About/About';
